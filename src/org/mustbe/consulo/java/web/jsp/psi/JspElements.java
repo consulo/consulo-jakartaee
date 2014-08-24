dceb1d35f9fafa1874b856a23c5d724e3e4b535f
VISTALL <vistall.valeriy@gmail.com>
2014-08-24 20:20:33 +0400

    compilation

--- src/org/mustbe/consulo/java/web/jsp/psi/JspElements.java
+++ src/org/mustbe/consulo/java/web/jsp/psi/JspElements.java
@@ -6,8 +6,8 @@ import org.mustbe.consulo.java.web.jsp.psi.impl.JspDirectiveImpl;
 import org.mustbe.consulo.java.web.jsp.psi.impl.JspExpressionImpl;
 import org.mustbe.consulo.java.web.jsp.psi.impl.JspFragmentImpl;
 import org.mustbe.consulo.java.web.jsp.psi.impl.JspLineFragmentImpl;
+import com.intellij.psi.tree.ElementTypeAsPsiFactory;
 import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.IElementTypeAsPsiFactory;
 
 /**
  * @author VISTALL
@@ -15,13 +15,13 @@ import com.intellij.psi.tree.IElementTypeAsPsiFactory;
  */
 public interface JspElements
 {
-	IElementType DIRECTIVE = new IElementTypeAsPsiFactory("DIRECTIVE", JspLanguage.INSTANCE, JspDirectiveImpl.class);
+	IElementType DIRECTIVE = new ElementTypeAsPsiFactory("DIRECTIVE", JspLanguage.INSTANCE, JspDirectiveImpl.class);
 
-	IElementType ATTRIBUTE = new IElementTypeAsPsiFactory("ATTRIBUTE", JspLanguage.INSTANCE, JspAttributeImpl.class);
+	IElementType ATTRIBUTE = new ElementTypeAsPsiFactory("ATTRIBUTE", JspLanguage.INSTANCE, JspAttributeImpl.class);
 
-	IElementType FRAGMENT = new IElementTypeAsPsiFactory("FRAGMENT", JspLanguage.INSTANCE, JspFragmentImpl.class);
+	IElementType FRAGMENT = new ElementTypeAsPsiFactory("FRAGMENT", JspLanguage.INSTANCE, JspFragmentImpl.class);
 
-	IElementType LINE_FRAGMENT = new IElementTypeAsPsiFactory("LINE_FRAGMENT", JspLanguage.INSTANCE, JspLineFragmentImpl.class);
+	IElementType LINE_FRAGMENT = new ElementTypeAsPsiFactory("LINE_FRAGMENT", JspLanguage.INSTANCE, JspLineFragmentImpl.class);
 
-	IElementType EXPRESSION = new IElementTypeAsPsiFactory("EXPRESSION", JspLanguage.INSTANCE, JspExpressionImpl.class);
+	IElementType EXPRESSION = new ElementTypeAsPsiFactory("EXPRESSION", JspLanguage.INSTANCE, JspExpressionImpl.class);
 }
