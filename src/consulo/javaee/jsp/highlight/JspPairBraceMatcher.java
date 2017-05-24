2973f6ded4fd7b5fe01f402f9b0042e1de79735e
VISTALL <vistall.valeriy@gmail.com>
2017-05-24 22:28:24 +0300

    struct

--- src/consulo/javaee/jsp/highlight/JspPairBraceMatcher.java
+++ src/consulo/javaee/jsp/highlight/JspPairBraceMatcher.java
@@ -16,9 +16,9 @@ public class JspPairBraceMatcher implements PairedBraceMatcher
 {
 	private static final BracePair[] ourBraces = new BracePair[]{
 			new BracePair(JspTokenType.JSP_DIRECTIVE_START, JspTokenType.JSP_DIRECTIVE_END, true),
-			new BracePair(JspTokenType.JSP_SCRIPTLET_START, JspTokenType.JSP_SCRIPTLET_END, false),
-			new BracePair(JspTokenType.JSP_EXPRESSION_START, JspTokenType.JSP_EXPRESSION_END, false),
-			new BracePair(JspTokenType.JSP_DECLARATION_START, JspTokenType.JSP_DECLARATION_END, false),
+			new BracePair(JspTokenType.JSP_SCRIPTLET_START, JspTokenType.JSP_SCRIPTLET_END, true),
+			new BracePair(JspTokenType.JSP_EXPRESSION_START, JspTokenType.JSP_EXPRESSION_END, true),
+			new BracePair(JspTokenType.JSP_DECLARATION_START, JspTokenType.JSP_DECLARATION_END, true),
 	};
 
 	@Override
@@ -36,6 +36,6 @@ public class JspPairBraceMatcher implements PairedBraceMatcher
 	@Override
 	public int getCodeConstructStart(PsiFile psiFile, int i)
 	{
-		return 0;
+		return i;
 	}
 }
