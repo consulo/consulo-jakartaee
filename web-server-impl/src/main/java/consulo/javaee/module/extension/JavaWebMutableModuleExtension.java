957d120c0d897168715eec65573712f457d7c565
VISTALL <vistall.valeriy@gmail.com>
2020-06-03 19:10:41 +0300

    compilation

--- web-server-impl/src/main/java/consulo/javaee/module/extension/JavaWebMutableModuleExtension.java
+++ web-server-impl/src/main/java/consulo/javaee/module/extension/JavaWebMutableModuleExtension.java
@@ -17,9 +17,13 @@
 package consulo.javaee.module.extension;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
+import consulo.disposer.Disposable;
 import consulo.module.extension.MutableModuleExtension;
 import consulo.roots.ModuleRootLayer;
+import consulo.ui.Component;
+import consulo.ui.annotation.RequiredUIAccess;
 
 /**
  * @author VISTALL
@@ -30,6 +34,14 @@ public class JavaWebMutableModuleExtension extends JavaWebModuleExtension implem
 	public JavaWebMutableModuleExtension(@Nonnull String id, @Nonnull ModuleRootLayer module)
 	{
 		super(id, module);
+	}
+
+	@RequiredUIAccess
+	@Nullable
+	@Override
+	public Component createConfigurationComponent(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
+	{
+		return null;
 	}
 
 	@Override
